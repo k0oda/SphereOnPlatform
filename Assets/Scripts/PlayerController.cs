7972c7f11833228e58ddeea17e4be1784a4c9d83
kukree <kulevgen32@gmail.com>
2020-01-17 22:53:55 +0600

    Add jumping to PlayerController script
    Implement isGrounded method

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody rb;
+    Collider collider;
     
     float velocityX, velocityY, velocityZ, groundCheckPoint;
     Vector3 movement;
@@ -14,11 +15,14 @@ public class PlayerController : MonoBehaviour
     public float cameraOffsetY = 2;
     public float cameraOffsetZ = -6;
     public float movementSpeed = 10;
+    public float jumpForce = 350;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
+        groundCheckPoint = collider.bounds.extents.y;
     }
 
     // Update is called once per frame
@@ -36,7 +40,21 @@ public class PlayerController : MonoBehaviour
         velocityX = Input.GetAxis("Horizontal");
         velocityZ = Input.GetAxis("Vertical");
 
+        if (Input.GetButtonDown("Jump") && isGrounded())
+        {
+            velocityY = jumpForce;
+        }
+        else
+        {
+            velocityY = 0;
+        }
+
         movement = new Vector3(velocityX * movementSpeed, velocityY, velocityZ * movementSpeed);
         rb.AddForce(movement);
     }
 
+    bool isGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, groundCheckPoint + 0.1f);
+    }
+}
